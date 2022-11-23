ca374547894fd6a7add532f0d473743d5b771e4d
sourov <sourov.jsr98@gmail.com>
2022-11-23 23:42:42 +0600

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layouts/Main';
+import Blog from './Pages/Blog/Blog';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <Signup></Signup>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
